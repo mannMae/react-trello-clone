c7b0c511163f4e5c41d62ec05515b88f1940247a
김재만 <daga4242@gmail.com>
2022-09-28 17:57:11 +0900

    recoil

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atom";
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+  const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+e.currentTarget.value);
+  };
+  const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setHours(+e.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+        value={hours}
+      />
+    </div>
+  );
+}
+
+export default App;
