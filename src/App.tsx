79df0b99e43a444c6c9d821a18d8700b9b9f2542
김재만 <daga4242@gmail.com>
2022-09-28 18:20:02 +0900

    beautiful-drag-n-drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { hourSelector, minuteState } from "./atom";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
@@ -11,21 +12,44 @@ function App() {
   const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
     setHours(+e.currentTarget.value);
   };
+  const onDragEnd = () => {};
   return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinutesChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input
-        onChange={onHoursChange}
-        type="number"
-        placeholder="Hours"
-        value={hours}
-      />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="one">
+        {(p) => (
+          <div>
+            <input
+              value={minutes}
+              onChange={onMinutesChange}
+              type="number"
+              placeholder="Minutes"
+            />
+            <input
+              onChange={onHoursChange}
+              type="number"
+              placeholder="Hours"
+              value={hours}
+            />
+            <ul ref={p.innerRef} {...p.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(p) => (
+                  <li ref={p.innerRef} {...p.draggableProps}>
+                    <span {...p.dragHandleProps}>1️⃣</span>1 11
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {(p) => (
+                  <li ref={p.innerRef} {...p.draggableProps}>
+                    <span {...p.dragHandleProps}>2️⃣</span> 222222
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
